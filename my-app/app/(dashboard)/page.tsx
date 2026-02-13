d55c3cf5d6e1d35c240f27bf057827319757d71d
Dhruv <dhruvjoshi1069@gmail.com>
2026-02-13 23:07:44 +0530

    some more changes in ui and other basic ui logics

--- my-app/app/(dashboard)/page.tsx
+++ my-app/app/(dashboard)/page.tsx
@@ -3,15 +3,18 @@ import { getCurrent } from "@/feature/auth/actions";
 import { redirect } from "next/navigation";
 
 import { CreateWorkspaceForm } from "@/feature/workspaces/components/create-workspace-form"
+import { getworkspaces } from "@/feature/workspaces/actions";
 
 
 export default async function Home() {
 
   const user = await getCurrent();
   if(!user) redirect("/sign-in")
-  return (
-   <div className="bg-neutral-500 p-4 h-full">
-    <CreateWorkspaceForm />
-   </div>
-  );
+
+    const workspaces  = await getworkspaces();
+    if(workspaces.total === 0){
+      redirect("/workspaces/create");
+    } else{
+      redirect(`/workspaces/${workspaces.documents[0].$id}`);
+    }
 };
--- my-app/components/workspace-switcher.tsx
+++ my-app/components/workspace-switcher.tsx
@@ -11,19 +11,24 @@ import {
     SelectValue,
 } from "@/components/ui/select";
 import { WorkspaceAvatar } from "@/feature/workspaces/components/workspace-avatar";
+import { useRouter } from "next/navigation";
+import { useWorkspaceId } from "@/feature/workspaces/hooks/use-workspace-id";
 
 
 export const WorkspaceSwitcher = ()=>{
-
+    const workspaceId = useWorkspaceId();
+    const router = useRouter();
     const {data:workspaces} = useGetWorkspaces();
-    
+    const onSelect = (id:string)=>{
+        router.push(`/workspaces/${id}`);
+    };
     return (
         <div className="flex flex-col gap-y-2">
             <div className="flex items-center justify-between">
                 <p className="text-xs uppercase text-neutral-500">Workspaces</p>
                 <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"/>
             </div>
-            <Select>
+            <Select onValueChange={onSelect} value={workspaceId}>
                 <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
                     <SelectValue placeholder="No workspace selected"/>
                 </SelectTrigger>
