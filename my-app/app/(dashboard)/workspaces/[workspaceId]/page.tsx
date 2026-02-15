dcbb67b2e7e0fe4edc5c389c25dda5c7377324d4
atharv <atharv.shawrikar.7@gmail.com>
2026-02-16 01:02:27 +0530

    invite system working

--- my-app/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ my-app/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,13 +1,23 @@
 import { getCurrent } from "@/feature/auth/queries";
 import { redirect } from "next/navigation";
 
-const WorkspaceIdPage = async () => {
+// 1. Define the type for the async params
+interface WorkspaceIdPageProps {
+  params: Promise<{ workspaceId: string }>;
+}
+
+const WorkspaceIdPage = async ({ 
+  params 
+}: WorkspaceIdPageProps) => {
     const user = await getCurrent();
-      if(!user) redirect("/sign-in")
+    if (!user) redirect("/sign-in");
+
+    const { workspaceId } = await params;
+
     return ( 
-    <div>
-        Workspace ID
-    </div> 
+        <div>
+            Workspace ID: 
+        </div> 
     );
 }
  
